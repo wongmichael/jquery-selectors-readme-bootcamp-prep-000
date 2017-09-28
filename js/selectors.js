6be2e1b0552ce3fde1cc597283fba65c84a01a83
Michael Wong <mik9080@gmail.com>
2017-09-28 18:41:31 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+function lastImageSelector(){
+  return $('img:last')
+}
+function ninjaBabySelector(){
+  $('#baby-ninja')
+}
+function divSelector(){
+  $('.pics')
+}
+function firstListItem(){
+  $('#pic-list ul li:first-child')
+}
